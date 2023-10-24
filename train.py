cfd3f8f3199e085e2f6cb3f001c35862fa6dbc8e
David Navarro <dcnavarros@unal.edu.co>
2023-10-24 16:50:38 -0500

    requirements updated

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
 import numpy as np
 import json
+
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
